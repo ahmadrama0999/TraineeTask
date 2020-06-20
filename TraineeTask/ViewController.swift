68df4696a7435eabcd51e3eea99655250bb147e1
Akhmad Ramin <ahmadrama0999@gmail.com>
2020-06-20 12:12:46 +0300

    done task

--- TraineeTask/ViewController.swift
+++ TraineeTask/ViewController.swift
@@ -9,70 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    
-    
-    var array: [String] = ["Ramin", "Alex", "Misha", "Viktor"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        print("STRUCT")
-        var studentS1 = StudentStruct()
-        print("S1" ,studentS1)
-        let studentS2 = studentS1
-        print("S2",studentS2)
-        studentS1.name = "Rama"
-        print("S1",studentS1)
-        print("S2",studentS2)
-        
-        print("Class")
-        let studentC1 = StudentClass()
-        print("C1" ,studentC1.name)
-        let studentC2 = studentC1
-        print("C2" ,studentC2.name)
-        studentC1.name = "Dani"
-        print("C1",studentC1.name)
-        print("C2",studentC2.name)
-        
-        
         
     }
     
-    @IBAction func sortCellAction(_ sender: Any) {
-        array.sort { (s1, s2) -> Bool in
-            return s1<s2
-        }
-        tableView.reloadData()
-    }
 }
     
 
-extension ViewController: UITableViewDelegate {
+// Example of using GENERIC
+struct Stack <Element> {
+    var values = [Element]()
     
-}
-
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
+    mutating func push(value: Element) {
+        values.append(value)
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SimpleCell") else { return UITableViewCell()}
-        cell.textLabel?.text = array[indexPath.row]
-        return cell
+    mutating func pop() {
+        values.removeLast()
+    }
+}
+
+struct Queue <Element> {
+    var values = [Element]()
+    
+    mutating func add(value: Element) {
+        values.append(value)
     }
     
+    mutating func remove() {
+        values.removeFirst()
+    }
 }
-
-struct StudentStruct {
-    var name = "Vika"
-}
-
-class StudentClass {
-    var name = "Viktor"
-}
-
